4b3803dd407813eb9afd0fe719f1bd34bb6d61ba
mortza mansory <mortzamansory12@gmail.com>
2025-02-17 11:17:56 -0500

    Update waf_rule.py

--- test/services/waf_rule.py
+++ test/services/waf_rule.py
@@ -23,13 +23,30 @@ class WAFRules:
     def check_waf_enabled(self):
         return self.is_mod_security_enabled()
 
-    def load_rule(self, rule):
-        if not self.check_waf_enabled():
-            raise Exception("WAF is offline. Please enable ModSecurity first.")
-        result = lib.loadRule(rule.encode('utf-8'))
-        if not result:
-            raise Exception(f"Failed to load rule: {rule}")
-        return result
+    def load_rule(self, rule_name):
+      if not self.check_waf_enabled():
+        raise Exception("WAF is offline. Please enable ModSecurity first.")
+    
+      rules_directory = "/usr/local/nginx/rules/"
+      rule_file_path = os.path.join(rules_directory, rule_name)
+    
+      if not os.path.exists(rule_file_path):
+        return {"status": "error", "message": f"Rule file {rule_name} not found."}
+
+      try:
+         with open(rule_file_path, 'r') as rule_file:
+             rule_content = rule_file.read()
+         return {
+             "status": "success", 
+             "message": f"Rule {rule_name} loaded successfully.", 
+             "rule_content": rule_content
+         }
+
+      except Exception as e:
+        return {
+            "status": "error", 
+            "message": f"Error loading rule {rule_name}: {str(e)}"
+        }
 
     def show_modsec_rules(self):
         result = lib.showModSecRules()
@@ -37,11 +54,36 @@ class WAFRules:
             print("Failed to fetch ModSecurity rules.")
             return None
 
-        rules = ctypes.cast(result, ctypes.c_char_p).value.decode('utf-8')  # Decode the C string to Python string
+        rules = ctypes.cast(result, ctypes.c_char_p).value.decode('utf-8') 
         rule_list = rules.splitlines()
         lib.free(result)
         return rule_list
+    
+    def update_rule(self, rule_name, new_content):
+        if not self.check_waf_enabled():
+            raise Exception("WAF is offline. Please enable ModSecurity first.")
+        
+        rules_directory = "/usr/local/nginx/rules/"
+        rule_file_path = os.path.join(rules_directory, rule_name)
+        
+        if not os.path.exists(rule_file_path):
+            return {"status": "error", "message": f"Rule file {rule_name} not found."}
 
+        try:
+            with open(rule_file_path, 'w') as rule_file:
+                rule_file.write(new_content)
+            return {
+                "status": "success",
+                "message": f"Rule {rule_name} updated successfully.",
+                "rule_content": new_content
+            }
+
+        except Exception as e:
+            return {
+                "status": "error", 
+                "message": f"Error updating rule {rule_name}: {str(e)}"
+            }
+        
     def create_new_rule(self, title, body):
         rules_directory = "/usr/local/nginx/rules/"
 
