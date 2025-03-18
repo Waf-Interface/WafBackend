f1d97a6124f66596c3bb990e3c5530cd1b033c6e
mortza mansory <mortzamansory12@gmail.com>
2025-03-18 12:25:00 -0400

    Update user_model.py

--- test/models/user_model.py
+++ test/models/user_model.py
@@ -1,6 +1,11 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Enum
 from sqlalchemy.sql import func
 from services.database.database import Base
+import enum
+
+class UserRole(enum.Enum):
+    admin = "admin"
+    user = "user"
 
 class User(Base):
     __tablename__ = "users"
@@ -11,5 +16,6 @@ class User(Base):
     first_name = Column(String, nullable=True) 
     last_name = Column(String, nullable=True)  
     email = Column(String, nullable=True)       
+    role = Column(Enum(UserRole), nullable=False) 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
