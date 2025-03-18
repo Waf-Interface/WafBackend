0346e5c0d90f97a02c89ce51a89a7bba8a53864a
mortza mansory <mortzamansory12@gmail.com>
2025-03-18 12:24:39 -0400

    Update app.py
    Including the inject dependencies fr authorization JWT.
    Including dataset creating.
    Including users service.
    Including system router.

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 from api.auth.auth import auth_router
@@ -6,15 +6,25 @@ from api.site.deploy import deploy_router
 from api.system.system_info import system_info_router
 from api.websocket.websocket import websocket_router
 from api.waf.waf_rule import router as waf_rule_router  
+from api.system.system import router as system_router  
 from api.waf.waf_manager import router as waf_manager
 from api.system.loger import router as loger_router  
 from api.waf.waf_crs import router as waf_setup_router  
+from services.auth.generate_secret_key import generate_secret_key  
 from services.backup_service import BackupService  
 from api.log.nginx_log import router as nginx_log  
+from services.database.database import engine
+from models.user_model import User
+from services.auth.verify_token import verify_token  
+from api.users.users import user_router 
+
+User .metadata.create_all(bind=engine)
 
 backup_service = BackupService()
 
 app = FastAPI()
+
+generate_secret_key()
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,13 +35,16 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
-app.include_router(deploy_router)
-app.include_router(system_info_router)
-app.include_router(websocket_router)
-app.include_router(waf_manager, prefix="/waf", tags=["waf"])
-app.include_router(waf_rule_router, prefix="/waf", tags=["waf"])
-app.include_router(loger_router)  
-app.include_router(waf_setup_router, prefix="/waf", tags=["waf"])  
-app.include_router(nginx_log) 
+app.include_router(user_router) 
+app.include_router(deploy_router, dependencies=[Depends(verify_token)]) 
+app.include_router(system_info_router, dependencies=[Depends(verify_token)])
+app.include_router(websocket_router) 
+app.include_router(system_router, prefix="/sys", tags=["sys"], dependencies=[Depends(verify_token)]) 
+app.include_router(waf_manager, prefix="/waf", tags=["waf"], dependencies=[Depends(verify_token)])  
+app.include_router(waf_rule_router, prefix="/waf", tags=["waf"], dependencies=[Depends(verify_token)])  
+app.include_router(loger_router, dependencies=[Depends(verify_token)]) 
+app.include_router(waf_setup_router, prefix="/waf", tags=["waf"], dependencies=[Depends(verify_token)]) 
+app.include_router(nginx_log, dependencies=[Depends(verify_token)]) 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8081)
