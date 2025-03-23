6714574f676bc6397bbcda2f72ef6d7bbd793f1f
mortza mansory <mortzamansory12@gmail.com>
2025-03-23 12:10:24 -0400

    Update database.py
    Adding access session.

--- test/services/database/database.py
+++ test/services/database/database.py
@@ -2,8 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./users.db"  
+DATABASE_URL = "sqlite:///./users.db"
+ACCESS_DB_URL = "sqlite:///./access.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+access_engine = create_engine(ACCESS_DB_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+AccessSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=access_engine)
+
 Base = declarative_base()
+AccessBase = declarative_base()
