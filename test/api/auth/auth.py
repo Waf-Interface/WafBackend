67dfe3838428e41ffb4ea9719b0c9bfe67b40642
mortza mansory <mortzamansory12@gmail.com>
2025-03-26 14:28:28 -0400

    Update auth.py

--- test/api/auth/auth.py
+++ test/api/auth/auth.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, HTTPException, Body
-import secrets
-
-from models.auth_models import LoginRequest
-from services.auth.auth_service import login_service, verify_otp_service
+from fastapi import APIRouter, HTTPException, Depends
+from models.auth_models import LoginRequest, VerifyOTPRequest
+from services.auth.auth_service import login_service, verify_otp_service, logout_service
+from services.auth.jwt import verify_token  #
 
 auth_router = APIRouter()
 
@@ -11,5 +10,10 @@ async def login(request: LoginRequest):
     return await login_service(request)
 
 @auth_router.post("/verify_otp")
-async def verify_otp(session_id: str = Body(...), otp: int = Body(...)):
-    return await verify_otp_service(session_id, otp)
+async def verify_otp(request: VerifyOTPRequest): 
+    return await verify_otp_service(request)
+
+@auth_router.post("/logout")
+async def logout(current_user: dict = Depends(verify_token)): 
+    username = current_user["sub"] 
+    return await logout_service(username)
