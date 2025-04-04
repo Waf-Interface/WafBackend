1a4c57f5a2e0604957040fbef372d58a26f46349
mortza mansory <mortzamansory12@gmail.com>
2025-04-04 20:25:56 +0330

    Delete redis_client.py

--- redis_client.py
+++
@@ -1,6 +0,0 @@
-import redis
-from redis.client import Redis
-from config import REDIS_URL
-
-# Initialize Redis client
-redis_client: Redis = redis.Redis.from_url(REDIS_URL, decode_responses=True) # type: ignore
